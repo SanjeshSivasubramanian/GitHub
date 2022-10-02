d5cdb4a504cc153e20ae71d28e542e309217672b
SanjeshSivasubramanian <sanjeshcivil@gmail.com>
2022-10-02 13:05:59 +0530

    Day1

---
+++ src/org/Github/Gitsample1.java
@@ -0,0 +1,12 @@
+package org.Github;
+
+public class Gitsample1 {
+	private void test() {
+		
+		System.out.println("siva");
+	}
+public static void main(String[] args) {
+	Gitsample1 g = new Gitsample1();
+	g.test();
+}
+}
