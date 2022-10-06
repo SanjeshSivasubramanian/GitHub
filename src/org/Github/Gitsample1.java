aa1068eae30e5dcae9cbc317e8476d70fcaf2f46
SanjeshSivasubramanian <sanjeshcivil@gmail.com>
2022-10-06 16:50:30 +0530

    stage1

--- src/org/Github/Gitsample1.java
+++ src/org/Github/Gitsample1.java
@@ -5,6 +5,10 @@ public class Gitsample1 {
 		
 		System.out.println("siva");
 	}
+ private void test2() {
+	 System.out.println("sanjay check 1 ");
+ }
+	
 public static void main(String[] args) {
 	Gitsample1 g = new Gitsample1();
 	g.test();
